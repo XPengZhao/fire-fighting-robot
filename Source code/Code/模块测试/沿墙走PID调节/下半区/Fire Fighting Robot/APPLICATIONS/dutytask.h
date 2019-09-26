609439b610cb8d342c388278fd5d21e493c5faea
zhao <30794695+a-sleepy-cat@users.noreply.github.com>
2019-09-26 17:51:48 +0800

    change structure

---
+++ Source code/Code/模块测试/沿墙走PID调节/下半区/Fire Fighting Robot/APPLICATIONS/dutytask.h
@@ -0,0 +1,11 @@
+#ifndef DUTYTASK_H
+#define DUTYTASK_H
+
+#include "stm32f10x.h"
+
+void taskloop(void);
+void Duty_20ms(void);
+void Duty_40ms(void);
+void Duty_80ms(void);
+void Duty_400ms(void);
+#endif
--- Source code/代码部分/Fire Fighting Robot(RELEASE)/搜救机器人 Arduino/Readme.md
+++
@@ -1,4 +0,0 @@
-# Library
-
-必须将MsTimer2文件夹拷贝到Arduino安装目录下的libraries文件夹里，如
-D:\Program Files (x86)\Arduino\libraries
